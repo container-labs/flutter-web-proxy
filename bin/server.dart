acfa679f1f9ec9113ce0dc358e5ef4ec75de0283
Will <will@containerlabs.io>
2021-11-30 00:58:03 -0800

    basic proxy

--- bin/server.dart
+++ bin/server.dart
@@ -2,32 +2,23 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:convert';
 import 'dart:io';
 
+import 'package:http/http.dart' as dart_http;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart' as shelf_router;
 import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
 Future main() async {
-  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
-  // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
   // See https://pub.dev/documentation/shelf/latest/shelf/Cascade-class.html
-  final cascade = Cascade()
-      // First, serve files from the 'public' directory
-      .add(_staticHandler)
-      // If a corresponding file is not found, send requests to a `Router`
-      .add(_router);
+  final cascade = Cascade().add(_staticHandler).add(_router);
 
   // See https://pub.dev/documentation/shelf/latest/shelf_io/serve.html
   final server = await shelf_io.serve(
-    // See https://pub.dev/documentation/shelf/latest/shelf/logRequests.html
-    logRequests()
-        // See https://pub.dev/documentation/shelf/latest/shelf/MiddlewareExtensions/addHandler.html
-        .addHandler(cascade.handler),
+    logRequests().addHandler(cascade.handler),
     InternetAddress.anyIPv4, // Allows external connections
     port,
   );
@@ -35,30 +26,57 @@ Future main() async {
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-// Serve files from the file system.
-final _staticHandler =
-    shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
+final client = dart_http.Client();
 
-// Router instance to handler requests.
+// Serve files from the file system.
 final _router = shelf_router.Router()
-  ..get('/helloworld', _helloWorldHandler)
+  ..get('/maps/api/place/autocomplete/json', _placesHandler)
+  ..get('/maps/api/place/details/json', _detailsHander)
   ..get(
     '/time',
     (request) => Response.ok(DateTime.now().toUtc().toIso8601String()),
-  )
-  ..get('/sum/<a|[0-9]+>/<b|[0-9]+>', _sumHandler);
-
-Response _helloWorldHandler(Request request) => Response.ok('Hello, World!');
-
-Response _sumHandler(request, String a, String b) {
-  final aNum = int.parse(a);
-  final bNum = int.parse(b);
-  return Response.ok(
-    const JsonEncoder.withIndent(' ')
-        .convert({'a': aNum, 'b': bNum, 'sum': aNum + bNum}),
-    headers: {
-      'content-type': 'application/json',
-      'Cache-Control': 'public, max-age=604800',
-    },
   );
+
+// Router instance to handler requests.
+final _staticHandler =
+    shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
+
+Future<Response> _detailsHander(Request request) async {
+  Map<String, String> params = request.requestedUri.queryParameters;
+
+  final response = await client.get(
+    Uri.https('maps.googleapis.com', '/maps/api/place/details/json', {
+      'place_id': params['input'],
+      'key': params['key'],
+      'sessiontoken': params['sessionToken']
+    }),
+  );
+
+  return Response.ok(response.body, headers: {
+    'content-type': 'application/json',
+    'Cache-Control': 'public, max-age=604800',
+    // the whole reason this proxy exists, to add the access-control header
+    'Access-Control-Allow-Origin': '*'
+  });
+}
+
+// Can probably make a generic proxy but YOLO for now.
+Future<Response> _placesHandler(Request request) async {
+  Map<String, String> params = request.requestedUri.queryParameters;
+
+  final response = await client.get(
+    Uri.https('maps.googleapis.com', '/maps/api/place/autocomplete/json', {
+      'input': params['input'],
+      'types': params['types'],
+      'key': params['key'],
+      'sessiontoken': params['sessionToken']
+    }),
+  );
+
+  return Response.ok(response.body, headers: {
+    'content-type': 'application/json',
+    'Cache-Control': 'public, max-age=604800',
+    // the whole reason this proxy exists, to add the access-control header
+    'Access-Control-Allow-Origin': '*'
+  });
 }
